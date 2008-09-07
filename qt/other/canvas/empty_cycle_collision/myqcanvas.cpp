b49f4a16b3808b5558e5016c61580023e1715087
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2008-09-07 02:35:16 +0000

    完成renyang-learn/qt/other/canvas/empty_cycle_collision程式碼

--- qt/other/canvas/empty_cycle_collision/myqcanvas.cpp
+++ qt/other/canvas/empty_cycle_collision/myqcanvas.cpp
@@ -1,4 +1,6 @@
 
+#include <qregion.h>
+
 #include "myqcanvas.h"
 
 MyQCanvas::MyQCanvas(int w,int h):QCanvas(w,h)
@@ -8,17 +10,20 @@ MyQCanvas::MyQCanvas(int w,int h):QCanvas(w,h)
 
 void MyQCanvas::advance()
 {
-	if (my_r->collidesWith(my_p))
-	        my_t->setText("Collision");
-       	else
-               	my_t->setText("There is no Collision!");
+	QRegion region = QRegion(my_p->rect());
+	QRegion region1= region.eor(QRegion(my_p1->rect()));
 	
+	if (region1.contains(my_r->rect()))
+		my_t->setText("Collision");
+	else
+		my_t->setText("There is no Collision!");
 	QCanvas::advance();
 }
 
-void MyQCanvas::setAddress(QCanvasRectangle* p,QCanvasRectangle* r,QCanvasText* t)
+void MyQCanvas::setAddress(QCanvasRectangle* p,QCanvasRectangle* p1,QCanvasRectangle* r,QCanvasText* t)
 {
 	my_p = p;
+	my_p1= p1;
 	my_r = r;
 	my_t = t;
 }
