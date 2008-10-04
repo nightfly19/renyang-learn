3480156e6ff095f0d0b86b97c18b6dc4a3b99679
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2008-10-04 02:53:18 +0000

    把qt/other/canvas/continue_line/改為左鍵是畫線，右鍵是刪除線

--- trunk/qt/other/canvas/continue_line/view.cpp
+++ trunk/qt/other/canvas/continue_line/view.cpp
@@ -23,25 +23,38 @@ View::View(QCanvas &canvas):QCanvasView(&canvas)
 
 void View::contentsMousePressEvent(QMouseEvent *e)
 {
-	if (!started){
-		end = new QPoint(e->x(),e->y());
-		started=true;
-		#ifdef MYDEBUG	
-		cout << end->x() << " and " << end->y() << endl;
-		cout << "you are the first time" << endl;
-		#endif
+	if (e->button()==LeftButton){
+		if (!started){
+			end = new QPoint(e->x(),e->y());
+			started=true;
+			#ifdef MYDEBUG	
+			cout << end->x() << " and " << end->y() << endl;
+			cout << "you are the first time" << endl;
+			#endif
+		}
+		else
+		{
+			start = end;
+			end = new QPoint(e->x(),e->y());
+			#ifdef MYDEBUG
+			cout << "start is (" << start->x() << "," << start->y() << ") and end is (" << end->x() << "," << end->y() << ")" << endl;
+			#endif
+			QCanvasLine *i = new QCanvasLine(canvas());
+			i->setPoints(start->x(),start->y(),end->x(),end->y());
+			i->setPen(blue);
+			i->setZ(2);
+			i->show();
+		}
 	}
-	else
+	else if (e->button()==RightButton)
 	{
-		start = end;
-		end = new QPoint(e->x(),e->y());
-		#ifdef MYDEBUG
-		cout << "start is (" << start->x() << "," << start->y() << ") and end is (" << end->x() << "," << end->y() << ")" << endl;
-		#endif
-		QCanvasLine *i = new QCanvasLine(canvas());
-		i->setPoints(start->x(),start->y(),end->x(),end->y());
-		i->setPen(blue);
-		i->setZ(2);
-		i->show();
+		started=false;
+		QCanvasItemList list=canvas()->allItems();
+		for (QCanvasItemList::Iterator it=list.begin();it!=list.end();it++)
+		{
+			if (*it){
+				delete *it;
+			}
+		}
 	}
 }
