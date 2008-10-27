b05a7733286f011535868fcb4a6a2c93b30f79e3
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2008-10-27 15:51:18 +0000

    如何使用char組合成string

---
+++ c++/other/char2string/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char **argv){
+	char word[]={'H','e','l','l','o','\0'};
+	char string1[]="World";
+	cout << "word is " << word << " and string1 is " << string1 << endl;
+	return 0;
+}
+
