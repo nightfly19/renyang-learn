5c4eb6e18d9db9b681d3337c009e3a025b87084d
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2008-11-22 02:30:27 +0000

    add some c++ code, and update the update.sh

---
+++ trunk/c++/other/pause/pause.h
@@ -0,0 +1,7 @@
+#ifndef _PAUSE_H
+#define _PAUSE_H
+
+#include <stdio.h>
+#define PAUSE printf("Press any key to continue..."); fgetc(stdin);
+
+#endif // _PAUSE_H
---
+++ trunk/c++/other/sizeof/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main() { 
+    cout << "\n型態\t大小(bytes)"; 
+    cout << "\nint\t" << sizeof(int); 
+    cout << "\nlong\t" << sizeof(long); 
+    cout << "\nfloat\t" << sizeof(float); 
+    cout << "\ndouble\t" << sizeof(double); 
+    cout << "\nchar\t" << sizeof(char);
+    cout << "\n";
+
+    return 0; 
+}
