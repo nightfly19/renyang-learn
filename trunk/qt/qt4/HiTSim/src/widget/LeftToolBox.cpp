7f338167e728de00ce2979d4a8f511252e6eea0b
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-01-16 09:25:31 +0000

    修改LeftBox，加入多個toolbox

--- trunk/qt/qt4/HiTSim/src/widget/LeftToolBox.cpp
+++ trunk/qt/qt4/HiTSim/src/widget/LeftToolBox.cpp
@@ -2,12 +2,51 @@
 #include "LeftToolBox.h"
 
 #include <QLabel>
+#include <QButtonGroup>
+#include <QGridLayout>
+#include <QToolButton>
 
 LeftToolBox::LeftToolBox(QWidget *parent):QToolBox(parent) {
+
+	addItem(createCarPage(),tr("Car list"));
+}
+
+QWidget *LeftToolBox::createCarPage() {
+	
+
+	QGridLayout *carLayout = new QGridLayout;
+	buttonGroup = new QButtonGroup(this);
+	buttonGroup->setExclusive(true);
+	carLayout->addWidget(createCarButton(":car_b.png",buttonGroup,1),0,0);
+	carLayout->addWidget(createCarButton(":car_g.png",buttonGroup,1),0,1);
+	carLayout->addWidget(createCarButton(":car_r.png",buttonGroup,1),1,0);
+
+	carLayout->setRowStretch(2,10);
+	carLayout->setColumnStretch(2,10);
+
+	QWidget *carWidget = new QWidget;
+	carWidget->setLayout(carLayout);
+
 	setSizePolicy(QSizePolicy(QSizePolicy::Maximum, QSizePolicy::Ignored));
 	setMinimumWidth(200);	// 設定最小值為200
-	addItem(new QLabel("second",this),"Road_list");
-	addItem(new QLabel("first",this),"Car_list");
-	addItem(new QLabel("third",this),"RoadSide_unit");
+
+	return carWidget;
+}
+
+QWidget *LeftToolBox::createCarButton(const QString &name,QButtonGroup *group,const int id) {
+	
+	QToolButton *carButton = new QToolButton;
+	carButton->setCheckable(true);
+	carButton->setIcon(QIcon(QPixmap(name).scaled(30,30,Qt::KeepAspectRatio)));
+	carButton->setIconSize(QSize(50,50));
+
+	group->addButton(carButton, id);
+
+	QGridLayout *layout = new QGridLayout;
+	layout->addWidget(carButton,0,0,Qt::AlignHCenter);
+
+	QWidget *widget = new QWidget;
+	widget->setLayout(layout);
+	return widget;
 }
 
--- trunk/qt/qt4/HiTSim/src/widget/LeftToolBox.h
+++ trunk/qt/qt4/HiTSim/src/widget/LeftToolBox.h
@@ -4,11 +4,19 @@
 
 #include <QToolBox>
 
+class QButtonGroup;
+
 // 此類別主要是配置左邊的快速列,目前主要是建立道路與車子
 class LeftToolBox:public QToolBox {
 Q_OBJECT
 	public:
 		LeftToolBox(QWidget *parent=0);
+	
+	private:
+		QWidget *createCarPage();
+		QWidget *createCarButton(const QString &name,QButtonGroup *group,const int id);
+
+		QButtonGroup *buttonGroup;
 };
 
 #endif // LEFTTOOLBOX_H
