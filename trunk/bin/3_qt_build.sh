9f52f56c13f029b0e3f2675dfd524093909fa621
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-01-21 10:50:14 +0000

    便新svn相關sh檔

--- trunk/bin/3_qt_build.sh
+++ trunk/bin/3_qt_build.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-qmake-qt3 -project;qmake-qt3;make
+echo "=================================================================================="
+echo "if you don't want to create .pro file, you can add any parameter after instruction"
+echo "=================================================================================="
+
+if [ "$1" == "" ]; then
+	qmake-qt3 -project
+fi
+
+qmake-qt3;make
