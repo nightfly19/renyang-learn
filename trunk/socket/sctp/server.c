b1d5b12d7462983768553339fff423b6747e5145
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-02-06 16:36:03 +0000

    update sctp code

---
+++ trunk/socket/sctp/server.c
@@ -0,0 +1,12 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+	printf("Hello World!!\n");
+
+	return 0;
+}
