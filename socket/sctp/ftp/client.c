9b0f9e9e0f1aafd4c611610d8affefe02bfe542a
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-02-09 14:44:57 +0000

    complete the one-to-one stcp

--- socket/sctp/ftp/client.c
+++ socket/sctp/ftp/client.c
@@ -19,11 +19,13 @@ int ServerSock;
 
 //===========================define function======================
 int ConnectToServer(char *);
+int RecvMsg(int,char *);
+int SendMsg(int,char *);
 //================================================================
 int main(int argc,char *argv[])
 {
 	// variable
-	char ipaddr[15]="127.0.0.1";
+	char ipaddr[]="127.0.0.1";
 	char recvbuffer[FILEBUFFERSIZE];
 	char sendbuffer[FILEBUFFERSIZE];
 	char filebuffer[FILEBUFFERSIZE];
@@ -32,17 +34,75 @@ int main(int argc,char *argv[])
 	int AddrLen = sizeof(SockAddr);
 
 	// initialize
-	memset(ipaddr,0,sizeof(ipaddr));
+	// memset(ipaddr,0,sizeof(ipaddr));
 
 	ServerSock = ConnectToServer(ipaddr);
+	printf("waiting for welcome....\n");
+	while(1) {
+		fgets(sendbuffer,FILEBUFFERSIZE,stdin);
+		SendMsg(ServerSock,sendbuffer);
+		if (strncmp(sendbuffer,"exit",strlen("exit"))==0)
+		{
+			break;
+		}
+	}
 
-	printf("#end#");
+	printf("#end#\n");
 	return 0;
 }
 
 //==================function implement===============================
 int ConnectToServer(char *ipaddr)
 {
-	return 0;
+	int ServerSock;
+	struct sockaddr_in ServAddr;
+	int TransportType;
+	int ret;
+	struct sctp_event_subscribe events;
+	struct sctp_initmsg initmsg;
+
+	// setting
+	memset(&ServAddr,0,sizeof(ServAddr));
+	ServAddr.sin_family = PF_INET;
+	ServAddr.sin_addr.s_addr = inet_addr(ipaddr);
+	ServAddr.sin_port = htons(HTTP_PORT);
+	TransportType = SOCK_STREAM;
+
+	// open a socket
+	ServerSock = socket(PF_INET,TransportType,IPPROTO_SCTP);
+	if (ServerSock < 0)
+	{
+		printf("Error to open socket!!!\n");
+		exit(-1);
+	}
+
+	bzero(&initmsg,sizeof(initmsg));
+	initmsg.sinit_num_ostreams = MAXFILETRANSFER;
+	initmsg.sinit_max_instreams = MAXFILETRANSFER;
+	initmsg.sinit_max_attempts = MAXFILETRANSFER - 1;
+	setsockopt(ServerSock,IPPROTO_SCTP,SCTP_INITMSG,&initmsg,sizeof(initmsg));
+	if (connect(ServerSock,(struct sockaddr *)&ServAddr,sizeof(ServAddr)) < 0)
+	{
+		printf("Error to Connect socket!!!\n");
+		exit(-1);
+	}
+
+	// Enable receipt of SCTP Snd/Rcv Data via sctp_recvmsg
+	bzero(&events,sizeof(events));
+	events.sctp_data_io_event = 1;
+	ret = setsockopt(ServerSock,IPPROTO_SCTP,SCTP_EVENTS,(const void*)&events,sizeof(events));
+	printf("ret:%d\n",ret);
+	return ServerSock;
+}
+
+int RecvMsg(int skt,char *msg)
+{
+	bzero(msg,FILEBUFFERSIZE);
+	return recv(skt,msg,FILEBUFFERSIZE,0);
+}
+
+int SendMsg(int skt,char *msg)
+{
+	return send(skt,msg,strlen(msg),0);
 }
 //===================================================================
