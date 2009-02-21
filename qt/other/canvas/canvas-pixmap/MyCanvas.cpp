e6d4db83eff3b143d9b3558e4fbd4dc59e8b715d
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-02-21 09:04:20 +0000

    accomplish using canvaspixmap

--- qt/other/canvas/canvas-pixmap/MyCanvas.cpp
+++ qt/other/canvas/canvas-pixmap/MyCanvas.cpp
@@ -2,5 +2,10 @@
 #include "MyCanvas.h"
 
 MyCanvas::MyCanvas(int w,int h):QCanvas(w,h) {
-	// do nothing
+	pixmap = new QCanvasPixmap("cater1.png");
+	array = new QCanvasPixmapArray();
+	array->setImage(0,pixmap);
+	sprite = new QCanvasSprite(array,this);
+	sprite->show();
 }
+
