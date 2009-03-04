1461d2415ebdf3c2d1555163c672b86aed1fae1d
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-03-04 14:32:21 +0000

    總算成功的讓QSocketNotifier不會一直跑了 原來是被觸發後,要刪除QSocketNotifier這一個物件

--- trunk/camera/QtOpenCV/qt3/image-doublebuffer/bad_tcp/client/MyCameraWindow.cpp
+++ trunk/camera/QtOpenCV/qt3/image-doublebuffer/bad_tcp/client/MyCameraWindow.cpp
@@ -1,7 +1,11 @@
 
 #include "MyCameraWindow.h"
+#include <memory.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-MyCameraWindow::MyCameraWindow(QWidget *parent):QWidget(parent)
+MyCameraWindow::MyCameraWindow(QWidget *parent):QWidget(parent),packageindex(0)
 {
 	resize(640,480);
 	QImage temp(640,480,32);
@@ -14,8 +18,8 @@ MyCameraWindow::MyCameraWindow(QWidget *parent):QWidget(parent)
 	// memset(&pixel,0,sizeof(struct imagedata));
 	// setCameraImage(pixel);
 	client_socket = new ClientSocket("127.0.0.1",30000);
-	// sn = new QSocketNotifier(client_socket->get_connfd(),QSocketNotifier::Read,this);
-	// QObject::connect(sn,SIGNAL(activated(int)),this,SLOT(Recvdata()));
+	sn = new QSocketNotifier(client_socket->get_connfd(),QSocketNotifier::Read,this);
+	QObject::connect(sn,SIGNAL(activated(int)),this,SLOT(Recvdata()));
 }
 
 MyCameraWindow::~MyCameraWindow()
@@ -39,15 +43,29 @@ void MyCameraWindow::setCameraImage(struct imagedata &structimage)
 
 void MyCameraWindow::Recvdata()
 {
-	/*
+	if (sn) delete sn;	// 若不刪掉的話,會一直觸發這一個function,因為,還有資料沒有接收到
 	char filebuffer[MAXRECV];
 	memset(filebuffer,0,MAXRECV);
+	struct imagedata image;
+	memset(&image,0,sizeof(struct imagedata));
+	char *char_image = (char*) &image;
 	int Readbyte=0;
 	Readbyte=*client_socket>>filebuffer;
-	if (strncmp(filebuffer,"#over#",strlen("#over#"))==0)
-		printf("byt!!\n");*/
-	// if (sn) delete sn;
-	//sn = new QSocketNotifier(client_socket->get_connfd(),QSocketNotifier::Read,this);
-	//QObject::connect(sn,SIGNAL(activated(int)),this,SLOT(Recvdata()));
-	//printf("x");
+	while (strncmp(filebuffer,"#over#",strlen("#over#"))!=0)
+	{
+		Readbyte = *client_socket>>filebuffer;
+		// strcpy(char_image+MAXRECV*packageindex,filebuffer);
+		// memcpy(char_image+MAXRECV*packageindex,filebuffer,MAXRECV);
+		packageindex++;
+	}
+		// printf("%c\n",image.b_pixel[220][220]);
+		// printf("%d\n",packageindex);
+		printf("byt!!\n");
+		packageindex=0;
+		/*
+		FILE *fp;
+		fp = fopen("after","wb");
+		fwrite(&image,1,sizeof(struct imagedata),fp);
+		fclose(fp);
+		*/
 }
