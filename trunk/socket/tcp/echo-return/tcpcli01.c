42260f7d1aa4f9159ab3062a87d317350e841b9f
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-03-19 16:23:04 +0000

    增加select的程式

---
+++ trunk/socket/tcp/echo-return/tcpcli01.c
@@ -0,0 +1,75 @@
+//======================include file============================
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//==============================================================
+
+//======================define variable=========================
+#include "global.h"
+//==============================================================
+
+//======================define function=========================
+void str_cli(FILE *fp,int sockfd);
+//==============================================================
+
+//======================main function===========================
+int main(int argc,char **argv)
+{
+	int sockfd;
+	struct sockaddr_in servaddr;
+	int ret_value=0;
+
+	if (argc != 2) {
+		printf("usage: tcpcli <IPaddress>");
+		exit(-1);
+	}
+
+	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	if (sockfd == -1) {
+		printf("socket error\n");
+		exit(-1);
+	}
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	ret_value = inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+	if (ret_value == -1) {
+		printf("inet_pton error\n");
+		exit(-1);
+	}
+
+	ret_value = connect(sockfd,(struct sockaddr *) &servaddr,sizeof(servaddr));
+	if (ret_value == -1) {
+		perror("connect error\n");
+		exit(-1);
+	}
+
+	str_cli(stdin,sockfd);
+
+	exit(0);
+}
+//==============================================================
+
+//======================function implement======================
+void str_cli(FILE *fp,int sockfd)
+{
+	char sendline[MAXLINE],recvline[MAXLINE];
+	int ret_value=0;
+
+	while (fgets(sendline,MAXLINE,fp) != NULL) {
+		ret_value = write(sockfd,sendline,strlen(sendline));
+
+		memset(recvline,0,sizeof(recvline));
+		if (read(sockfd,recvline,MAXLINE) ==0) {
+			printf("str_cli: server terminated prematurely");
+			exit(-1);
+		}
+
+		fputs(recvline,stdout);
+	}
+}
