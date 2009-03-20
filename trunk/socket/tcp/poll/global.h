8cbf43834d7b7deaab6f663ac8d4d88fed64eb54
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-03-20 13:52:47 +0000

    增加一個poll的tcp:client-server:程式

---
+++ trunk/socket/tcp/poll/global.h
@@ -0,0 +1,16 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#define SERV_PORT 9877
+#define LISTENQ 1024
+#define MAXLINE 4096
+#define OPEN_MAX 256
+#define POLLRDNORM	0x0040
+
+#endif
+
+#ifndef INFTIM
+
+#define INFTIM (-1)
+
+#endif
