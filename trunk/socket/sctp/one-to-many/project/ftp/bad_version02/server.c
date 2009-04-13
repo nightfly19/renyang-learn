d79350700270b432031e29fa9f8d6d4dd6997e16
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-04-13 17:50:03 +0000

    還沒有完成sctp的ftp

---
+++ trunk/socket/sctp/one-to-many/project/ftp/bad_version02/server.c
@@ -0,0 +1,60 @@
+#include "unp.h"
+
+int main(int argc,char **argv)
+{
+	int sock_fd,msg_flags;
+	char readbuf[BUFFSIZE];
+	char filename[BUFFSIZE];
+	struct sockaddr_in servaddr, cliaddr;
+	struct sctp_sndrcvinfo sri;
+	struct sctp_event_subscribe evnts;
+	int stream_increment = 1;	// 每一次stream no增加1
+	socklen_t len;
+	size_t rd_sz;
+	FILE *fp;
+
+	sock_fd = Socket(AF_INET,SOCK_SEQPACKET,IPPROTO_SCTP);
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	Bind(sock_fd,(SA *) &servaddr,sizeof(servaddr));
+
+	// 把所有的event事件均打開
+	SctpTurnOffAllEvent(sock_fd);
+
+	Listen(sock_fd,LISTENQ);
+	printf("Start waiting...\n");
+	for (;;) {
+		len = sizeof(struct sockaddr_in);
+		rd_sz = Sctp_recvmsg(sock_fd,readbuf,sizeof(readbuf),(SA *) &cliaddr,&len,&sri,&msg_flags);
+		if (msg_flags & MSG_NOTIFICATION) {
+			print_notification(readbuf);
+			continue;
+		}
+		sri.sinfo_stream++;
+		if (sri.sinfo_stream>9)
+			sri.sinfo_stream=0;
+
+		printf("You input is %s",readbuf);
+		if (strncmp(readbuf,"#getfile ",strlen("#getfile "))==0) {
+			printf("You want to get file\n");
+			bzero(filename,BUFFSIZE);
+			strcpy(filename,readbuf + strlen("#getfile "));
+			printf("%s",filename);
+			if ((fp=fopen(filename,"rb"))==NULL)
+				Sctp_sendmsg(sock_fd,"open file error",strlen("open file error"),(SA *) &cliaddr,sizeof(cliaddr),0,0,sri.sinfo_stream,0,0);
+			else {
+				Sctp_sendmsg(sock_fd,"open ok",strlen("open ok"),(SA *) &cliaddr,sizeof(cliaddr),0,0,sri.sinfo_stream,0,0);
+				rd_sz = Sctp_recvmsg(sock_fd,readbuf,sizeof(readbuf),(SA *) &cliaddr,&len,&sri,&msg_flags);
+				if (strncmp(readbuf,"open file ok",strlen("open file ok"))==0)
+					SendFile(sock_fd,fp,(SA *) &cliaddr,sizeof(cliaddr),sri.sinfo_stream);
+			}
+			continue;
+		}
+		bzero(&readbuf,sizeof(readbuf));
+		Sctp_sendmsg(sock_fd,"ok",strlen("ok"),(SA *) &cliaddr,sizeof(cliaddr),0,0,sri.sinfo_stream,0,0);
+	}
+	return 0;
+}
