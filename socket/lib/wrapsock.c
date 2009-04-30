5b4b8b5a6cf7b7fd332d0977b3e45891b0d689c9
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-04-30 13:35:25 +0000

    建立一個例子可以同時讓udp與tcp使用同一個port同時運行 與多個tcp使用同一個port的設定方法

--- socket/lib/wrapsock.c
+++ socket/lib/wrapsock.c
@@ -60,3 +60,17 @@ int Select(int nfds,fd_set *readfds,fd_set *writefds,fd_set *execptfds,struct ti
 		err_sys("select error");
 	return (n);
 }
+
+void Sendto(int fd, const void *ptr, size_t nbytes,int flags, const struct sockaddr *sa, socklen_t salen)
+{
+	if (sendto(fd,ptr,nbytes,flags,sa,salen) != (ssize_t) nbytes)
+		err_sys("sendto error");
+}
+
+ssize_t Recvfrom(int fd,void *ptr,size_t nbytes,int flags, struct sockaddr *sa, socklen_t *salenptr)
+{
+	ssize_t n;
+	if ((n=recvfrom(fd,ptr,nbytes,flags,sa,salenptr))<0)
+		err_sys("recvfrom error");
+	return (n);
+}
