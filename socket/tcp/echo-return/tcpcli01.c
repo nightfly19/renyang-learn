0393e44834d792a9d9993910d06141222db58642
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-03 07:42:02 +0000

    修改tcp的例子，加入註解

--- socket/tcp/echo-return/tcpcli01.c
+++ socket/tcp/echo-return/tcpcli01.c
@@ -20,6 +20,7 @@ void str_cli(FILE *fp,int sockfd);
 int main(int argc,char **argv)
 {
 	int sockfd;
+	// servaddr代表在client端表示server端的socket
 	struct sockaddr_in servaddr;
 	int ret_value=0;
 
@@ -28,21 +29,27 @@ int main(int argc,char **argv)
 		exit(-1);
 	}
 
+	// 建立一個服務
 	sockfd = socket(AF_INET,SOCK_STREAM,0);
 	if (sockfd == -1) {
 		printf("socket error\n");
 		exit(-1);
 	}
 
+	// 填入server端的相關資料, ip address, port number
 	bzero(&servaddr,sizeof(servaddr));
+	// 填入server端要使用的協定
 	servaddr.sin_family = AF_INET;
+	// 填入server端要使用的port號
 	servaddr.sin_port = htons(SERV_PORT);
+	// 把string形式的ip address轉換成network的形式
 	ret_value = inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
 	if (ret_value == -1) {
 		printf("inet_pton error\n");
 		exit(-1);
 	}
 
+	// 連線到server端
 	ret_value = connect(sockfd,(struct sockaddr *) &servaddr,sizeof(servaddr));
 	if (ret_value == -1) {
 		perror("connect error\n");
@@ -61,15 +68,19 @@ void str_cli(FILE *fp,int sockfd)
 	char sendline[MAXLINE],recvline[MAXLINE];
 	int ret_value=0;
 
+	// 抓由使用者輸入的資料
 	while (fgets(sendline,MAXLINE,fp) != NULL) {
+		// 傳送以sendline記憶體位址為起點長度為strlen(sendline)到server端
 		ret_value = write(sockfd,sendline,strlen(sendline));
 
 		memset(recvline,0,sizeof(recvline));
+		// 由server端接收資料, 並寫入到記憶體位置recvline, 並限定最大長度為MAXLINE
 		if (read(sockfd,recvline,MAXLINE) ==0) {
 			printf("str_cli: server terminated prematurely");
 			exit(-1);
 		}
 
+		// 把資料送到使用者面前
 		fputs(recvline,stdout);
 	}
 }
--- socket/tcp/echo-return/tcpserv01.c
+++ socket/tcp/echo-return/tcpserv01.c
@@ -19,32 +19,54 @@ void str_echo(int sockfd);
 //======================main function===========================
 int main(int argc,char **argv)
 {
+	// 建立兩個記錄file descriptor的變數
+	// listenfd是server端用來觀察是否有新的連線進來
+	// connfd是在server端代表client端的file descriptor
+	// 要傳送資訊到client端均是透過connfd
 	int listenfd,connfd;
+	// childpid表示由主要的main process建立子process的id
 	pid_t childpid;
-	socklen_t clilen;
+	// servaddr是用來記錄要建立listenfd的server端的相關資訊, 像是使用的協定啦, port號啦, 要建立服務在哪一個ip上啦
+	// cliaddr則是用來記錄要建立connfd的client端的相關資料
+	// 把它當作一個平台來想像，socket file descriptor是平台上面服務
 	struct sockaddr_in cliaddr,servaddr;
+	// 宣告一個記錄cliaddr的長度的變數
+	socklen_t clilen;
 	int ret_value=0;
 
+	// 依所設定的通訊協定建立一個server端的socket
 	listenfd = socket(AF_INET,SOCK_STREAM,0);
 	if (listenfd == -1) {
 		perror("socket error");
 		exit(-1);
 	}
+	// 設定要在哪一個port與哪一個協定，哪一個ip上建立服務
 	memset(&servaddr,0,sizeof(servaddr));
+	// 使用哪一種協定
 	servaddr.sin_family = AF_INET;
+	// 要把服務架再所有本機端的ip上
+	// 若只要建在某一個ip上可以使用
+	// servaddr.sin_addr.s_addr = htonl("a.b.c.d");
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// 這一個服務要使用哪一個port上
 	servaddr.sin_port = htons(SERV_PORT);
 
+	// 把這一個socket服服綁在剛剛建立的平台上
 	ret_value = bind(listenfd,(struct sockaddr *) &servaddr,sizeof(servaddr));
+	// bind失敗
 	if (ret_value == -1) {
 		perror("bind error");
 		exit(-1);
 	}
 
+	// 這一個socket可以提供多少個連線進來
 	ret_value = listen(listenfd,LISTENQ);
 
 	for (;;) {
 		clilen = sizeof(cliaddr);
+		// 等待對方的連線
+		// 這個時候cliaddr的內容值是空的
+		// 當有連線進來時, 才會依實際上對方的ip...的填入資料
 		connfd = accept(listenfd,(struct sockaddr *) &cliaddr,&clilen);
 
 		if ((childpid == fork()) == 0) {	// 等於0表示是child process
@@ -65,7 +87,9 @@ void str_echo(int sockfd)
 	char buf[MAXLINE];
 	int ret_value=0;
 again:
+	// read表示由sockfd，接收到資料，要準備放到buf為起始的位置中, MAXLINE設定為最大接收大小
 	while((n = read(sockfd,buf,MAXLINE))>0) {
+		// write表示回傳由buf開始的起始位置, 且長度為n的資料送回去給對方
 		ret_value = write(sockfd,buf,n);
 	}
 	if (n<0 && errno==EINTR) {
