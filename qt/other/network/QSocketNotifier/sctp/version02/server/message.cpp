cd00d2d3e76ad7d6b4afab47ea653155d18e2e43
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-06 13:05:14 +0000

    修正sctp使用QSocketNotifier的錯誤

---
+++ qt/other/network/QSocketNotifier/sctp/version02/server/message.cpp
@@ -0,0 +1,51 @@
+#include "message.h"
+#include <fcntl.h>
+
+message::message()
+{
+	sn = NULL;
+	sock_fd = socket(AF_INET,SOCK_SEQPACKET,IPPROTO_SCTP);
+	if (sock_fd == -1)
+	{
+		qWarning("socket error");
+		exit(-1);
+	}
+	if (fcntl(sock_fd,F_SETFL,O_NONBLOCK)<0)
+	{
+		qWarning("PID_UP::fcntl:O_NONBLOCK error");
+		exit(-1);
+	}
+	sn = new QSocketNotifier(sock_fd,QSocketNotifier::Read,this);
+	connect(sn,SIGNAL(activated(int)),this,SLOT(printsys()));
+
+	memset(&servaddr,0,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(1793);
+
+	ret_value = ::bind(sock_fd,(struct sockaddr *) &servaddr,sizeof(servaddr));
+	if (ret_value == -1)
+	{
+		qWarning("bind error");
+		exit(-1);
+	}
+	ret_value = listen(sock_fd,5);
+	if (ret_value == -1)
+	{
+		qWarning("listen error");
+		exit(-1);
+	}
+	qWarning("start waiting...");
+}
+
+void message::printsys()
+{
+	len = sizeof(struct sockaddr_in);
+	rd_sz = sctp_recvmsg(sock_fd,readbuf,sizeof(readbuf),(struct sockaddr *) &cliaddr,&len,&sri,&msg_flags);
+	ret_value = sctp_sendmsg(sock_fd,readbuf,rd_sz,(struct sockaddr *) &cliaddr,len,sri.sinfo_ppid,sri.sinfo_flags,sri.sinfo_stream,0,0);
+	if (ret_value == -1)
+	{
+		qWarning("sctp_sendmsg error");
+		exit(-1);
+	}
+}
