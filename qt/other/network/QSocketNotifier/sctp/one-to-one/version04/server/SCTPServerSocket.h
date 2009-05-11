538ae9733c44fb751c17882de3e0f46870521ca1
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-11 08:34:25 +0000

    確認是否用connect也可以達到connectx的功能, 一次連到多個address

---
+++ qt/other/network/QSocketNotifier/sctp/one-to-one/version04/server/SCTPServerSocket.h
@@ -0,0 +1,35 @@
+#ifndef SCTPSERVERSOCKET_H
+#define SCTPSERVERSOCKET_H
+
+#include <qsocketnotifier.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/sctp.h>
+
+class SCTPServerSocket:public QObject
+{
+	Q_OBJECT
+	public:
+		SCTPServerSocket();
+		SCTPServerSocket(Q_UINT16, int backlog=1,QObject *parent=0,const char *name=0);
+	private:
+		QSocketNotifier *sctp_newconnection_notification;
+		QSocketNotifier *sctp_notification;
+		int listenfd;
+		int connfd;
+		struct sockaddr_in serv_addr;
+		struct sctp_initmsg initmsg;
+		int sendMsg(QString);
+
+		int Sctp_getladdrs(int, sctp_assoc_t);
+		int Sctp_getpaddrs(int,sctp_assoc_t);
+
+	signals:
+		void newConnection(int);
+
+	public slots:
+		int SCTPServerAccept(int);
+		int recvMsg(int);
+};
+
+#endif
