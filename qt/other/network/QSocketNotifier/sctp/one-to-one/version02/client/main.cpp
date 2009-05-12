0edaa05f0238aaf5b1a9e975d10109ce27298197
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-12 13:37:08 +0000

    修改為可以在執行檔案時輸入server ip

--- qt/other/network/QSocketNotifier/sctp/one-to-one/version02/client/main.cpp
+++ qt/other/network/QSocketNotifier/sctp/one-to-one/version02/client/main.cpp
@@ -3,11 +3,16 @@
 
 int main(int argc,char **argv)
 {
+	if (argc < 2) {
+		qWarning("%s serverip",argv[1]);
+		return 0;
+	}
+
 	QApplication app(argc,argv,false);
 
 	SCTPClientSocket *client = new SCTPClientSocket();
 
-	client->connectToHost("127.0.0.1",2000);
+	client->connectToHost(argv[1],2000);
 
 	return app.exec();
 }
