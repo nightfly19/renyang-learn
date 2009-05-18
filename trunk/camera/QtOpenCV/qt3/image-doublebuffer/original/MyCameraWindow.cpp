e50cd03a5c22c59e0a998cde664ab69c3ba605f0
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-18 03:41:26 +0000

    修改image-doublebuffer, 不會造成閃爍

--- trunk/camera/QtOpenCV/qt3/image-doublebuffer/original/MyCameraWindow.cpp
+++ trunk/camera/QtOpenCV/qt3/image-doublebuffer/original/MyCameraWindow.cpp
@@ -8,11 +8,14 @@ MyCameraWindow::MyCameraWindow(CvCapture *cam,QWidget *parent):QWidget(parent) {
 	layout->addWidget(cvwidget);
 	resize(500,400);
 
+	// 每100ms執行timerEvent()
 	startTimer(100);
 
 }
 
 void MyCameraWindow::timerEvent(QTimerEvent*) {
+	// 讀取一個frame
 	IplImage *image=cvQueryFrame(camera);
+	// 把讀取到的frame送到cvwidget顯示
 	cvwidget->putImage(image);
 }
--- trunk/camera/QtOpenCV/qt3/image-doublebuffer/original/main.cpp
+++ trunk/camera/QtOpenCV/qt3/image-doublebuffer/original/main.cpp
@@ -9,14 +9,19 @@
 #include "MyCameraWindow.h"
 
 int main(int argc,char **argv) {
+	/* start capturing frames from camera: index = camera_index + domain_offset (CV_CAP_*) */
+	// 0     // autodetect
 	CvCapture *camera = cvCreateCameraCapture(0);
 	// 確定camera必為true, 若為false則會終止程式, 並且印出是在哪一個檔案出現錯誤
 	assert(camera);
-	// 取得一個frame
+	// Just a combination of cvGrabFrame and cvRetrieveFrame
+	// 也就是取得一個frame啦
 	IplImage *image=cvQueryFrame(camera);
 	// 確認回傳的frame是沒有問題的
 	assert(image);
 
+	// pixel depth in bits
+	// 每一個pixel是用幾個bit來表示
 	printf("Image depth=%i\n",image->depth);
 	printf("Image nChannels=%i\n",image->nChannels);
 
@@ -27,6 +32,7 @@ int main(int argc,char **argv) {
 	app.setMainWidget(mainWin);
 	int retval = app.exec();
 
+	/* stop capturing/reading and free resources */
 	cvReleaseCapture(&camera);
 	return retval;
 }
