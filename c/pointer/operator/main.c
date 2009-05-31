4cea1e50e1aad3b501cc3a5beb4b015d94299b26
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-05-31 03:02:40 +0000

    增加一個指標運算的例子

--- c/pointer/operator/main.c
+++ c/pointer/operator/main.c
@@ -6,5 +6,7 @@ int main(int argc,char **argv)
 	printf("%x\n",dataPrt);
 	printf("%x\n",dataPrt-1);
 	printf("%x\n",(unsigned short int *) dataPrt-1);
+	// 這裡要注意一下, 後面的括號是先用char *作運算, 再轉成(unsigned short int *), 所以, 位址只有減少1 byte
+	printf("%x\n",(unsigned short int *) (dataPrt-1));
 	return 0;
 }
