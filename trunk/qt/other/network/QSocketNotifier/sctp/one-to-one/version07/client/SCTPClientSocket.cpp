0993035525fef074cc376f26a9dbb935d0e4c824
superporter2001 <superporter2001@e6a1191f-3756-0410-b840-13f34102b90a>
2009-06-01 08:02:39 +0000

    增加一個例子, 可以透過QSocketNotifier偵測到事件

---
+++ trunk/qt/other/network/QSocketNotifier/sctp/one-to-one/version07/client/SCTPClientSocket.cpp
@@ -0,0 +1,123 @@
+#include <arpa/inet.h>
+
+#include "SCTPClientSocket.h"
+
+SCTPClientSocket::SCTPClientSocket(QObject *parent,const char *name):QObject(parent,name)
+{
+	int ret;
+	struct sctp_initmsg initmsg;
+	connSock = -1;
+	sctp_notification = NULL;
+
+	connSock = ::socket(AF_INET, SOCK_STREAM,IPPROTO_SCTP);
+	if (connSock == -1) {
+		perror("socket error");
+		exit(1);
+	}
+
+	SctpSocketHandler::SctpSetMaxStream(connSock,5);
+	SctpSocketHandler::SctpSetNoDelay(connSock);
+	// 設定SCTP_NODELAY
+
+	SctpSocketHandler::SctpSetRtoMax(connSock,10000);
+	SctpSocketHandler::SctpSetRtoMin(connSock,1000);
+	// 宣控所有的事件
+	SctpSocketHandler::SctpTurnOnAllEvent(connSock);
+
+	memset(&initmsg,0,sizeof(initmsg));
+
+	initmsg.sinit_num_ostreams = 5;
+	initmsg.sinit_max_instreams = 5;
+	initmsg.sinit_max_attempts = 4;
+
+	ret = setsockopt(connSock,IPPROTO_SCTP,SCTP_INITMSG,&initmsg,sizeof(initmsg));
+	if (ret < 0) {
+		perror("setsockopt error");
+		exit(1);
+	}
+}
+
+void SCTPClientSocket::connectToHost(const QString &host, Q_UINT16 port)
+{
+	struct sockaddr_in servaddr;
+	int ret;
+
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
+	servaddr.sin_addr.s_addr = inet_addr(host.latin1());
+
+	ret = ::connect(connSock,(struct sockaddr *) &servaddr,sizeof(servaddr));
+	if (ret == -1) {
+		perror("connect error");
+		exit(1);
+	}
+	sctp_notification = new QSocketNotifier(connSock,QSocketNotifier::Read,this,"sctp_notification");
+	connect(sctp_notification,SIGNAL(activated(int)),this,SLOT(recvMsg(int)));
+	userinput = new QSocketNotifier(0,QSocketNotifier::Read,this,"userinput");
+	connect(userinput,SIGNAL(activated(int)),this,SLOT(getMsg(int)));
+	qWarning("connectToHost success!");
+}
+
+int SCTPClientSocket::SCTPSendmsg(const void *msg,size_t len,struct sockaddr *to,socklen_t tolen, uint32_t ppid,uint32_t flags,uint16_t stream_no, uint32_t timetolive, uint32_t context)
+{
+	int ret;
+	ret = sctp_sendmsg(connSock, msg,len,to,tolen,ppid,flags,stream_no,timetolive,context);
+	if (ret < 0) {
+		perror("sctp_sendmsg error");
+		exit(-1);
+	}
+	qWarning("send %d bytes",ret);
+	return ret;
+}
+
+int SCTPClientSocket::sendMsg(QString str)
+{
+	int ret;
+	ret = sctp_sendmsg(connSock,str.latin1(),str.length(),(struct sockaddr *) NULL,0,0,0,0,100,0);
+	if (ret == -1) {
+		perror("sendMsg error");
+		exit(-1);
+	}
+	qWarning("Send %d bytes",ret);
+	return ret;
+}
+
+// 有新的資料送過來
+int SCTPClientSocket::recvMsg(int s)
+{
+	int ret;
+	int msg_flags;
+	char recvbuffer[1024];
+	bzero(recvbuffer,1024);
+	ret = sctp_recvmsg(s,recvbuffer,1024,(struct sockaddr *) NULL,0,(struct sctp_sndrcvinfo *) NULL, &msg_flags);
+	if (ret == -1) {
+		perror("sctp_recvmsg error");
+		exit(-1);
+	}
+	else if (ret == 0) {
+		delete sctp_notification;
+		sctp_notification = NULL;
+		close(s);
+		emit serverclosed();
+		qWarning("peer endpoint close the connection");
+	}
+	if (msg_flags & MSG_NOTIFICATION)
+	{
+		qWarning("there is some notification");
+		SctpSocketHandler::emitSctpEvent(recvbuffer);
+		return ret;
+	}
+	qWarning("recv %d bytes",ret);
+	qWarning("%s",recvbuffer);
+	
+	return ret;
+}
+
+void SCTPClientSocket::getMsg(int)
+{
+	char getline[1024];
+	bzero(getline,1024);
+	gets(getline);
+	sendMsg(QString(getline));
+}
---
+++ trunk/qt/other/network/QSocketNotifier/sctp/one-to-one/version07/server/SCTPServerSocket.cpp
@@ -0,0 +1,168 @@
+#include "SCTPServerSocket.h"
+
+SCTPServerSocket::SCTPServerSocket()
+{
+	sctp_newconnection_notification = NULL;
+}
+
+SCTPServerSocket::SCTPServerSocket(Q_UINT16 port,int backlog, QObject *parent,const char *name):QObject(parent,name)
+{
+	int ret;
+
+	sctp_newconnection_notification = NULL;
+	sctp_notification = NULL;
+	listenfd = ::socket(AF_INET,SOCK_STREAM,IPPROTO_SCTP);
+	if (listenfd == -1) {
+		perror("ERROR opening socket");
+		exit(1);
+	}
+	
+	::bzero(&serv_addr,sizeof(struct sockaddr_in));
+
+	initmsg.sinit_num_ostreams = 5;
+	initmsg.sinit_max_instreams = 5;
+	initmsg.sinit_max_attempts = 4;
+	ret = ::setsockopt(listenfd,IPPROTO_SCTP,SCTP_INITMSG,&initmsg,sizeof(initmsg));
+	if (ret < 0) {
+		perror("ERROR setsockopt");
+		exit(1);
+	}
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(port);
+
+	if ((::bind(listenfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)))==-1) {
+		perror("Bind error");
+		exit(1);
+	}
+
+	// listen - listen for socket connections and limit the queue of incoming connections
+	if (::listen(listenfd,backlog)==-1) {
+		perror("listen error");
+		exit(1);
+	}
+
+	sctp_newconnection_notification = new QSocketNotifier(listenfd,QSocketNotifier::Read,this,"sctp_newconnection_notification");
+	connect(sctp_newconnection_notification,SIGNAL(activated(int)),this,SLOT(SCTPServerAccept(int)));
+	qWarning("the serversock is %d",listenfd);
+	qWarning("Accepting connections ...");
+}
+
+// 有新連線進來啦
+int SCTPServerSocket::SCTPServerAccept(int s)
+{
+	int connfd;
+	struct sockaddr_in cli_addr;
+	socklen_t client_len;
+	client_len = sizeof(cli_addr);
+	connfd = ::accept(s,(struct sockaddr *) &cli_addr,&client_len);
+	if (connfd == -1) {
+		perror("accept error");
+		exit(-1);
+	}
+
+	SctpSocketHandler::SctpSetMaxStream(connfd,5);
+	// 設定為SCTP_NODELAY
+	SctpSocketHandler::SctpSetNoDelay(connfd);
+
+	SctpSocketHandler::SctpSetRtoMax(connfd,10000);
+	SctpSocketHandler::SctpSetRtoMin(connfd,1000);
+	// 把所有的事件偵測都打開
+	SctpSocketHandler::SctpTurnOnAllEvent(connfd);
+
+	emit newConnection(connfd);
+	sctp_notification = new QSocketNotifier(connfd,QSocketNotifier::Read,this,"sctp_notification");
+	connect(sctp_notification,SIGNAL(activated(int)),this,SLOT(recvMsg(int)));
+	qWarning("accept success!!");
+	qWarning("the connfd is %d",connfd);
+	return connfd;
+}
+
+// 當有新的資料進來時, 會執行這一個function
+int SCTPServerSocket::recvMsg(int s)
+{
+	int ret;
+	char recvbuffer[1024];
+	struct sctp_sndrcvinfo sinfo;
+	int msg_flags;
+	struct sockaddr_in peer;
+	socklen_t peerlen = sizeof(peer);
+	bzero(recvbuffer,1024);
+	ret = ::sctp_recvmsg(s,recvbuffer,1024,(struct sockaddr *) &peer,&peerlen,&sinfo, &msg_flags);
+	if (ret == -1) {
+		perror("sctp_recvmsg error");
+		exit(-1);
+	}
+	// 當有收到訊息, 但是長度為0, 那就表示對方離開了
+	else if (ret == 0) {
+		delete sctp_notification;
+		sctp_notification = NULL;
+		close(s);
+		qWarning("client close");
+		return ret;
+	}
+
+	if ( msg_flags & MSG_NOTIFICATION)
+	{
+		qWarning("there is some notification");
+		SctpSocketHandler::emitSctpEvent(recvbuffer);
+		return ret;
+	}
+	qWarning("recv %d bytes",ret);
+
+	// 顯示local端有多少個ip address
+	qWarning("the peer address is %s",inet_ntoa(peer.sin_addr));
+	qWarning("There are %d local address",Sctp_getladdrs(s, sinfo.sinfo_assoc_id));
+	qWarning("There are %d peer address",Sctp_getpaddrs(s,sinfo.sinfo_assoc_id));
+	qWarning("sctp_recvmsg success!!");
+	connfd = s;
+	qWarning("%s",recvbuffer);
+	sendMsg(QString("%1").arg(recvbuffer));
+	return ret;
+}
+
+int SCTPServerSocket::sendMsg(QString str)
+{
+	int ret;
+	ret = sctp_sendmsg(connfd,str.latin1(),str.length(),(struct sockaddr *) NULL,0,0,0,0,100,0);
+	if (ret == -1) {
+		perror("sctp_sendmsg error");
+		exit(-1);
+	}
+	qWarning("%s",str.latin1());
+	qWarning("sctp_sendmsg success");
+	qWarning("send %d bytes",ret);
+	return ret;
+}
+
+int SCTPServerSocket::Sctp_getladdrs(int sockfd, sctp_assoc_t id)
+{
+	int ret;
+	struct sockaddr *addrs;
+	ret = sctp_getladdrs(sockfd,id,&addrs);
+	if (ret == -1) {
+		perror("sctp_getladdrs error");
+		exit(-1);
+	}
+	else
+	{
+		sctp_freeladdrs(addrs);
+	}
+	return ret;
+}
+
+int SCTPServerSocket::Sctp_getpaddrs(int sockfd,sctp_assoc_t id)
+{
+	int ret;
+	struct sockaddr *addrs;
+	ret = sctp_getpaddrs(sockfd,id,&addrs);
+	if (ret == -1) {
+		perror("sctp_getpaddrs error");
+		exit(-1);
+	}
+	else
+	{
+		sctp_freepaddrs(addrs);
+	}
+	return ret;
+}
